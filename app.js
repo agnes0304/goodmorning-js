56b2a5bfa99e212be1d0d0b145b00014f9786a5e
agnes0304 <jiwoochoi0304@gmail.com>
2023-02-23 17:57:00 +0900

    conditionals

--- app.js
+++ app.js
@@ -17,8 +17,30 @@ const calculator = {
     },
 };
 
-calculator.add(100,50);
-calculator.minus(100,50);
-calculator.mul(100,50);
-calculator.div(100,50);
-calculator.power(100,2);
+// calculator.add(100,50);
+// calculator.minus(100,50);
+// calculator.mul(100,50);
+// calculator.div(100,50);
+// calculator.power(100,2);
+
+
+// const age = 20;
+// function calKorAge(age) {
+// 	return age + 2;
+// }
+
+// const korAge = calKorAge(age);
+// console.log(korAge);
+
+
+const age = parseInt(prompt("How old are you?"));
+
+if (isNaN(age) || age < 0){
+    console.log("Enter the age plz.");
+} else if (age < 18) {
+    console.log("You are too young.");
+} else if (age >= 18 && age < 70) {
+    console.log("Enjoy your drink!");
+} else if (age >= 70) {
+    console.log("It's okay, but let's exercise!");
+}
