4eab8f83d304a9e884c4873aaadf931ed7274f07
agnes0304 <jiwoochoi0304@gmail.com>
2023-02-23 17:08:18 +0900

    calculator

---
+++ app.js
@@ -0,0 +1,19 @@
+// calculator
+const calculator = {
+    add: function(a,b){
+        console.log(a+b);
+    },
+    minus: function(a,b){
+        console.log(a-b);
+    },
+    mul: function(a,b){
+        console.log(a*b);
+    },
+    div: function(a,b){
+        console.log(a/b);
+    },
+    power: function(a,b){
+        console.log(a**b);
+    },
+};
+
